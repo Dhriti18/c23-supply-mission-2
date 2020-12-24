13b3c0d8f3b2769f542fa4711b62d33499431436
Dhriti18 <73419650+Dhriti18@users.noreply.github.com>
2020-12-24 19:16:27 +0530

    Add files via upload

---
+++ Blues.js
@@ -0,0 +1,28 @@
+class Blues{
+    constructor (x,y,width,height){
+    
+    
+    
+    this.body=Bodies.rectangle(x,y,width,height,{isStatic:true});
+    this.width=width;
+    this.height=height;
+    
+    World.add(world,this.body);
+    
+    
+    
+    
+    
+    }
+    
+    display(){
+      var p=this.body.position;
+      fill("red");
+      rectMode(CENTER);
+    //rect(p.x,p.y,this.width,this.height);
+    
+    
+    
+    
+    }
+}
